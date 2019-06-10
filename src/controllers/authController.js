29972764ae2b6453ad6fba8f2697e82ef8d9d534
Lucas <lucas@10fc.com.br>
2019-06-10 16:07:23 -0300

    Login and Validate with Token

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,8 +1,20 @@
 const express = require('express');
 
 const User = require('../models/User');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const authConfig = require('../config/auth');
+
 
 const router = express.Router();
+
+
+function generateToken(params = {}){
+   return jwt.sign(params, authConfig.secret,{
+
+        expiresIn: 86400,
+    });
+}
 
 router.post('/register', async (req, res) => {
    
@@ -16,12 +28,34 @@ router.post('/register', async (req, res) => {
     user.password = undefined;
    
    
-    return res.send({ user });
+    return res.send({ user,token: generateToken({ id: user.id}), });
 
     
     } catch (err){
         return res.status(400).send({ erro: 'Registration Failed!' });
     }
+
+});
+
+router.post('/authenticate', async (req,res) => {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email }).select('+password');
+
+    if(!user)
+        return res.status(400).send({error: 'User Not Found !'});
+
+
+    if(!await bcrypt.compare(password, user.password))
+        return res.status(400).send({error: 'Invalid Password !'});
+
+    
+    user.password = undefined;
+   
+    res.send({ 
+    user, 
+    token: generateToken({ id: user.id}),
+});
 
 });
 
